5c3e1163d4b887cfc688941f54ac7f5475575202
Rendyas <rendyadisaputra@gmail.com>
2019-03-09 08:29:27 +0700

    add new README.MD

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var cordova: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +10,17 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+      if(typeof(cordova) != 'undefined'){
+        var success = function(result) {
+            alert(JSON.stringify(result, undefined, 2));
+          }
+          var failure = function(result) {
+            alert(JSON.stringify(result, undefined, 2));
+          }
+          cordova.plugins.HelloWorld.coolMethod({
+            _sMessage: "Hello Text"
+          }, success, failure);
+    }
   }
 
 }
